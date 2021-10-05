b4dc6923eb0539c6e2ef8fb96a82d10d0ed702b4
Daniel Burruchaga <dbs99@hotmail.com>
2021-10-05 12:10:20 +0200

    Test Starting

--- test/test.js
+++ test/test.js
@@ -1,89 +1,61 @@
 /* ----------------------------------------------------------------
  *   AUTHOR:        Daniel Burruchaga Sola 
- *   FILE:           Logica.js
+ *   FILE:           test.js
  *   DATE:           20/06/2021
  *   STATE:          DONE
  *  ---------------------------------------------------------------- */
-// ........................................................
-// mainTest1.js
-// ........................................................
-var assert = require("assert")
-// ........................................................
-// main ()
-// ........................................................
-describe("Test 1: insertar ", function () {
-    // ....................................................
-    // ....................................................
-    var laLogica = null
-    // ....................................................
-    // ....................................................
-    it("conectar a la base de datos", function (hecho) {
-        laLogica = new Logica(
-            "../bd/datos.bd",
-            function (err) {
-                if (err) {
-                    throw new Error("No he podido conectar con datos.db")
-                }
+const mongoose = require('mongoose');
+const IP_PUERTO = "http://localhost:8080/api";
+var request = require("request");
+
+//Importing sensor
+const Sensor = require('../Models/sensorModel');
+const assert = require('assert');
+
+var sensorController = require('../Controllers/sensorController');
+var measureController = require('../Controllers/measureController');
+
+//tell mongoose to use es6 implementation of promises
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/Proyect3A');
+mongoose.connection
+    .once('open', () => console.log('Connected!'))
+    .on('error', (error) => {
+        console.warn('Error : ', error);
+    });
+
+
+describe('Connecting to DDBB', () => {
+    it('Connect?', (done) => {
+        //Called hooks which runs before something.
+        mongoose.connection.collections.sensors.drop(() => {
+            //this function runs after the drop is completed
+            done(); //go ahead everything is done now.
+        });
+    });
+});
+
+describe('Sensors', () => {
+    it("probar POST /sensors", function (hecho) {
+
+        sensor = new Sensor({ type: 'Test', address: '123456789' });
+
+        request.post(
+            {
+                url: IP_PUERTO + "/sensors",
+                headers: { "User-Agent": "DanielBurru", "Content-Type": "application/json" },
+                body: JSON.stringify(sensor)
+            },
+            function (err, respuesta, carga) {
+                assert.equal(err, null, "¿ha habido un error?");
+                assert.equal(respuesta.statusCode, 200, "¿El código no es 200 (OK)");
+                assert.equal(JSON.parse(carga).message, "New sensor created!", "¿La carga no es OK");
                 hecho()
-            })
+            } // callback
+        ) // .post
     }) // it
-    // ....................................................
-    // ....................................................
-    it("borrar todas las filas", async function () {
-        await laLogica.borrarFilasDeTodasLasTablas()
-    }) // it
-    // ....................................................
-    // ....................................................
-    it("puedo insertar una persona",
-        async function () {
-            await laLogica.insertarPersona({
-                dni: "1234A",
-                nombre: "Pepe",
-                apellidos: "García Pérez"
-            })
-            var res = await laLogica.buscarPersonaConDNI("1234A")
-            assert.equal(res.length, 1, "¿no hay un resulado?")
-            assert.equal(res[0].dni, "1234A", "¿no es 1234A?")
-            assert.equal(res[0].nombre, "Pepe", "¿no es Pepe?")
-        }) // it
-       
-        it("puedo insertar otra persona",
-        async function () {
-            await laLogica.insertarPersona({
-                dni: "1234B",
-                nombre: "Maria",
-                apellidos: "Jesus Jose"
-            })
-            var res = await laLogica.buscarPersonaConDNI("1234B")
-            assert.equal(res.length, 1, "¿no hay un resulado?")
-            assert.equal(res[0].dni, "1234B", "¿no es 1234B?")
-            assert.equal(res[0].nombre, "Maria", "¿no es Maria?")
-        }) // it
-    // ....................................................
-    // ....................................................
-    it("no puedo insertar una persona con dni que ya está",
-        async function () {
-            var error = null
-            try {
-                await laLogica.insertarPersona({
-                    dni: "1234A",
-                    nombre: "Pepa",
-                    apellidos: "Pérez Pérez"
-                })
-            } catch (err) {
-                error = err
-            }
-            assert(error, "¿Ha insertado el dni que ya estaba 1234A? (¿No ha pasado por el catch()?")
-        }) // it
-    // ....................................................
-    // ....................................................
-    it("cerrar conexión a la base de datos",
-        async function () {
-            try {
-                await laLogica.cerrar()
-            } catch (err) {
-                // assert.equal( 0, 1, "cerrar conexión a BD fallada: " + err)
-                throw new Error("cerrar conexión a BD fallada: " + err)
-            }
-        }) // it
-}) // describe
+
+})
+
+
+
